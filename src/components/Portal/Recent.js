c9c3db5e771571e0017264a8efab118de44b5630
Jacobo <jacoborodicio@gmail.com>
2022-01-03 12:07:54 +0100

    style: changed main layout, moved to mobile first design pattern

--- src/components/Portal/Recent.js
+++ src/components/Portal/Recent.js
@@ -8,17 +8,17 @@ const GlobalContainer = styled('div')`
   overflow: auto;
   padding: 1rem;
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: 1fr;
   gap: 1rem;
-  @media only screen and (max-width: 80rem) {
-    grid-template-columns: repeat(3, 1fr);
-  };
-  @media only screen and (max-width: 50rem) {
+  @media only screen and (min-width: 30rem) {
     grid-template-columns: 1fr 1fr;
-  };
-  @media only screen and (max-width: 30rem) {
-    grid-template-columns: 1fr;
-  };
+  }
+  @media only screen and (min-width: 50rem) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media only screen and (min-width: 80rem) {
+    grid-template-columns: repeat(4, 1fr);
+  }
 `;
 const Recent = () => {
     return (
